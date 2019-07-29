c8615adc4fa2475d8d1acbe26cb2a162f3fa7cd2
RyadForGamessss <53405623+RyadForGamessss@users.noreply.github.com>
2019-07-29 21:09:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "ID حقك") return;
+  if (message.author.id !== "597519365629280277") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
